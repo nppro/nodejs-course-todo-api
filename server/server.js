30be449c7ed6d0060289531fcaef8ed4941eadc4
Nguyen Phuoc <phamnguyenphuoc@gmail.com>
2017-11-28 16:51:00 +0700

    Add find all todo and post new todo with express server and mongoose

--- server/server.js
+++ server/server.js
@@ -1,77 +1,45 @@
-var mongoose = require('mongoose');
+// add module express, body-parser
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
+var app = express();
 
-// check tính hợp lệ của dữ liệu theo mongoose validatior
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        // khai báo mặc định
-        default: false 
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to api todo design by Phuoc');
 });
 
-// var newTodo = new Todo({
-//     text: 'Cook dinner',
-//     completed: true
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Save todo');
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (error) => {
-//     console.log('Unable to save todo', error);
-// });
-
-// var otherTodo = new Todo({
-//     text: 'Feed the dog 3  ',
+app.get('/todos', (req, res) => {
+    Todo.find({'completedAt': {$gt:0}})
+    .then((doc, err) => {
+        if(err)
+            res.send(err);
+        res.send(doc);
+    })
     
-// });
+})
 
-// otherTodo.save().then((doc) => {
-//     console.log('save success other todo');
-//     console.log(doc);
-// }, (error) => {
-//     console.log('Unable to save other todo');
-// })
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed,
+        completedAt: req.body.completedAt
+    });
 
-//User
-// email - require it - trim it - set type - set min length of 1
-// password
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.send(err);
+    })
+    // console.log(req.body);
+})
 
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: [true,'Vui lòng nhập email'],
-        trim: true,
-        minlength: [1, 'Vui lòng nhập hơn 1 ký tự']
-    },
-    password: {
-        type: String,
-        required: true
-    }
-});
 
-var newUser = new User({
-    email:'test@mail.com',
-    password: '12345'
-});
-
-newUser.save().then((doc)=>{
-    console.log('Save to user');
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (err) => {
-    console.log('Unable to save user', err);
+app.listen(5000, () => {
+    console.log('Server run on port 5000');
 })
