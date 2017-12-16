f5a4caadfc7e4915c609a1d47f780ee6d7054803
Nguyen Phuoc <phamnguyenphuoc@gmail.com>
2017-12-16 11:03:34 +0700

    Update connection with mongoose new version

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true,
+  /* other options */
+});
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect();
 
 // truyền biến mongoose ra ngoài
-module.exports = {
-    mongoose: mongoose
-};
+module.exports = {mongoose};
