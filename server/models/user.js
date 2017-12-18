3bb4ab03e640c28c196016df79125c96bd451dfc
Nguyên Phước <phamnguyenphuoc@gmail.com>
2017-12-18 21:37:51 +0700

    Add method static findBytoken

--- server/models/user.js
+++ server/models/user.js
@@ -45,6 +45,7 @@ var userSchema = new mongoose.Schema({
     }]
 });
 
+// ghi đè lên hàm của mongoose trả về để thiết lập các giá trị muốn trả về cho client là gì
 userSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
@@ -57,12 +58,35 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
+    // tạo token với private key là nppro7
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'nppro7').toString();
 
     user.tokens.push({access, token});
 
     return user.save().then(() => {
         return token;
+    })
+}
+
+// phương thức  tĩnh khi thực hiện tìm và decode token để tìm ra user
+userSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try{
+        decoded = jwt.verify(token,'nppro7');
+    }catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // })
+        // hàm trả vè lỗi khi đoạn lỗi được bắt, phải được trả về bằng promise để hàm nhận bên kia có thể hứng được
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token':token,
+        'tokens.access': 'auth'
     })
 }
 
